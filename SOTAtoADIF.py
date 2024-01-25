1c6e3f476105460e45153b73cff0d37721a60d79
G5JDA <156024586+G5JDA@users.noreply.github.com>
2024-01-25 19:56:52 +0000

    add argparse

--- SOTAtoADIF.py
+++ SOTAtoADIF.py
@@ -27,15 +27,35 @@ See README.md or https://github.com/G5JDA/SOTAtoADIF or https://g5jda.uk
 Please check CONTRIBUTING.md if you'd like to improve / add to what this program can do.
 """
 import time
+import argparse
 
-from modules import sota_api
 from modules import sota_csv
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='SOTAtoADIF',
+        description='Convert SOTA CSV log files to ADIF.',
+        epilog='For more information see https://github.com/G5JDA/SOTAtoADIF')
+
+    parser.add_argument('log_path',
+                        help='path to SOTA CSV log file (activator/chaser log)')
+
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='suppress informational console output')
+
+    parser.add_argument('-c', '--chaser', action='store_true',
+                        help='process as a chaser log (changes behaviour of -s)')
+
+    parser.add_argument('-s', '--s2s', metavar='s2s_log_path',
+                        help='path to SOTA S2S CSV log file (adds other summit info to output for activator mode, '
+                             'removes S2S QSOs in chaser mode)')
+
+    args = parser.parse_args()
+
     # do stuff
-    #data = sota_api.summit_data_from_ref(input("Input summit ref: "))
-    #print("Summit locator: " + data.get("locator"))
+    # data = sota_api.summit_data_from_ref(input("Input summit ref: "))
+    # print("Summit locator: " + data.get("locator"))
     file = input("Path to log csv: ")
 
     t = time.time()
@@ -55,18 +75,3 @@ if __name__ == '__main__':
     print(t1)
     print(t2)
     print(t3)
-
-
-"""
-Thinking two modes:
-
-Activator (most useful as we can assume locator):
-Take input activator log (and optionally S2S log)
-Output merged adif (use S2S log to supplement activator log)
-
-Chaser (less useful, we can't assume locator):
-Take input chaser log (and optionally S2S log)
-Subtract S2S entries from Chaser log
-output as ADIF
-
-"""
