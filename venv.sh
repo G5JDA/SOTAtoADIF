b4c2d8fae3d1debcbc8505a9780a955b4618645c
G5JDA <156024586+G5JDA@users.noreply.github.com>
2024-03-15 21:40:54 +0000

    Add venv.sh

---
+++ venv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Check if venv is missing
+if [ ! -d ".venv" ]; then
+  echo "SOTAtoADIF Python venv does not exist, creating..."
+  # create python venv
+  python -m venv --prompt="SOTAtoADIF" .venv
+  # activate the venv
+  source .venv/bin/activate
+  # install pip requirements
+  pip install -r requirements.txt
+fi
+
+# Activate the venv
+source .venv/bin/activate
