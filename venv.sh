2374c3935cf0e1275f03985346bf42d7ecb2b4b9
G5JDA <156024586+G5JDA@users.noreply.github.com>
2024-03-15 21:52:01 +0000

    Fix python alias

--- venv.sh
+++ venv.sh
@@ -3,12 +3,12 @@
 if [ ! -d ".venv" ]; then
   echo "SOTAtoADIF Python venv does not exist, creating..."
   # create python venv
-  python -m venv --prompt="SOTAtoADIF" .venv
+  python3 -m venv --prompt="SOTAtoADIF" .venv
   # activate the venv
   source .venv/bin/activate
   # install pip requirements
   pip install -r requirements.txt
 fi
 
-# Activate the venv
+# activate the venv
 source .venv/bin/activate
